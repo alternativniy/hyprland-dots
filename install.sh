97db9a3333fa740b25032e848bb4cbe367e791ac
r.romantsov <mchack4@gmail.com>
2025-01-20 17:02:14 +0500

    добавлены стили для waybar и необходимые пакеты

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 mkdir ~/.alttmp
 cd ~/.alttmp
 
-sudo pacman -Syu
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay && makepkg -si
-cd ~/.alttmp
+yes | sudo pacman -Syu
 
-sudo pacman -Sy - < packages-pacman.txt
-sudo yay -Sy - < packages-yay.txt
+if ! command -v yay 2>&1 >/dev/null
+then
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay && makepkg -si
+    cd ~/.alttmp
+fi    
+
+yes | sudo pacman -Sy - < ./packages-pacman.txt
+yes | sudo yay -Sy - < ./packages-yay.txt
 
 yes | cp -r -f .config/* ~/.config/
 
