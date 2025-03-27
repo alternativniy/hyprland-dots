5e2b9aa89c271a326e0054d79676384d99956b2f
r.romantsov <mchack4@gmail.com>
2025-03-27 13:51:53 +0500

    feature: добавлен zen-browser и правила для него

--- dist/fedora/install.sh
+++ dist/fedora/install.sh
@@ -6,11 +6,14 @@ sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y copr enable atim/starship
 sudo dnf -y copr enable erikreider/SwayNotificationCenter
+sudo dnf copr enable sneexy/zen-browser
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 sudo dnf check-update
 sudo dnf -y install $(<./packages.txt)
+
+
 
 mkdir -p ~/.local/share/fonts
 
